a4a096aae10d7ef827b62052ba78934fe5a1ba9c
Melissa Rice <mmrice22@gmail.com>
2021-03-18 21:05:12 +0000

    get_melon by id works

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, session, render_template, redirect, flash
 import jinja2
 
 import melons
@@ -14,7 +14,7 @@ import melons
 app = Flask(__name__)
 
 # A secret key is needed to use Flask sessioning features
-app.secret_key = 'this-should-be-something-unguessable'
+app.secret_key = 'RANDOM SECRET KEY'
 
 # Normally, if you refer to an undefined variable in a Jinja template,
 # Jinja silently ignores this. This makes debugging difficult, so we'll
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id) # should be (melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -62,11 +62,16 @@ def show_shopping_cart():
 
     # TODO: Display the contents of the shopping cart.
 
+    
     # The logic here will be something like:
     #
     # - get the cart dictionary from the session
+    session.get["cart"] = {}
     # - create a list to hold melon objects and a variable to hold the total
     #   cost of the order
+    my_melons = []
+    total_cost = 0
+
     # - loop over the cart dictionary, and for each melon id:
     #    - get the corresponding Melon object
     #    - compute the total cost for that type of melon
